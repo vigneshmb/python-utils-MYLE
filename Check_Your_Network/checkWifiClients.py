ec928ea5b5c2e5a4e837a18b4ed6fc377f576d98
vigneshmb <vigneshmb2016@gmail.com>
2025-03-31 17:45:35 +0530

    Update checkWifiClients.py

--- Check_Your_Network/checkWifiClients.py
+++ Check_Your_Network/checkWifiClients.py
@@ -1,17 +1,33 @@
 import scapy.all as scapyObject
 
-def arpPingLocalIp(ip_addr):
-    arp_result = scapyObject.arping(ip_addr)
-    for device in arp_result[0]:
-        print(device[1].pdst,device[1].src)
-
-if __name__ == "__main__":
-    print("getting current ip address")
+def currIpAddress():
     currIp=scapyObject.get_if_addr(scapyObject.conf.iface)
-    print("getting current ip subnet")
+    return currIp
+
+def currIpSubnet(currIp):
     getSubnet=currIp.split(".")
     getSubnet.pop()
     temp='.'.join(getSubnet)
     currIpSubnet=f'{temp}.1/24'
+    return currIpSubnet
+
+
+def arpPingLocalIp(ip_addr):
+    arp_result = scapyObject.arping(ip_addr)
+    result = []
+    for device in arp_result[0]:
+        temp={}
+        temp['mac_address']=device[1].src
+        temp['ip_address']=device[1].pdst
+        result.append(temp)
+    return result
+
+
+if __name__ == "__main__":
+    print("getting current ip address")
+    currIp = currIpAddress()
+    print("getting current ip subnet")
+    currIpSubnet=currIpSubnet(currIp)
     print(currIpSubnet)
-    arpPingLocalIp(currIpSubnet)
+    print(arpPingLocalIp(currIpSubnet))
+    scapyObject.conf.ifaces.show(True)  # type: ignore
